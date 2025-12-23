d3785b1ff8abfc85d61da06aaf1e5447052f8c47
Mukesh s <smileymukesh2008@gmail.com>
2025-12-23 08:03:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+const texts = [
+  "Hey… today isn’t just a birthday 🎂",
+  "It’s the day the universe quietly upgraded ✨",
+  "Because YOU happened 💫",
+  "Another year. More magic. Same legend 👑"
+];
 
+let index = 0;
+const aiText = document.getElementById("aiText");
+
+function typeText(text) {
+  aiText.innerHTML = "";
+  let i = 0;
+  const interval = setInterval(() => {
+    aiText.innerHTML += text.charAt(i);
+    i++;
+    if (i >= text.length) clearInterval(interval);
+  }, 60);
+}
+
+typeText(texts[index]);
+
+document.getElementById("evolveBtn").addEventListener("click", () => {
+  index = (index + 1) % texts.length;
+  typeText(texts[index]);
+});
